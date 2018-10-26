ec0739426a72f34a368db08ce45570ee44d9776d
Sean king <sean7thomasking@gmail.com>
2018-10-26 04:28:37 +0100

    updated

---
+++ clean.sh
@@ -0,0 +1,4 @@
+#! /bin/sh
+rm -rf client-message-emitter-service
+rm -rf ingest-service
+rm -rf mock-blockchain-swagger-ui
--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #! /bin/sh
 git clone https://github.com/Blockchain-utility-applications/mock-blockchain-swagger-ui.git
 git clone https://github.com/Blockchain-utility-applications/ingest-service.git
+git clone https://github.com/Blockchain-utility-applications/client-message-emitter-service.git
 docker-compose build 
 docker-compose up
