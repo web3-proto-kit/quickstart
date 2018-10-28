3ea4764e2fbf010493bb4c61444f309b47426aad
Sean king <sean7thomasking@gmail.com>
2018-10-28 19:27:04 +0000

    added check in connect function

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 git clone https://github.com/Blockchain-utility-applications/mock-blockchain-swagger-ui.git
 git clone https://github.com/Blockchain-utility-applications/ingest-service.git
 git clone https://github.com/Blockchain-utility-applications/client-message-emitter-service.git
+git clone https://github.com/Blockchain-utility-applications/ui-table-view.git
 docker-compose build 
 docker-compose up
